e2f3f59592d7ad8dfd992dcc63e7f3e9b99527c1
Dhekra ZELAITI <dhekra.zelaiti@gmail.com>
2020-03-10 11:29:12 +0100

    Step 01: create skeleton of application

---
+++ telamaque-movies/src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        borderTop: '1px dashed gray',
+        textAlign: 'center',
+        padding: 10,
+        color: 'gray'
+    },
+    creatorName: {
+        fontWeight: 'bold'
+    }
+}));
+
+const Footer = () => {
+    const classes = useStyles();
+
+    return <div className={classes.root}>
+        By <span className={classes.creatorName}>Dhekra ZELAITI</span> &lt;dhekra.zelaiti@gmail.com&gt; | 06 37 27 38 77
+    </div>;
+}
+
+export default Footer;
