34aee2ace5c99ca912b5cc02ef22249878d3a04f
Dhekra ZELAITI <dhekra.zelaiti@gmail.com>
2020-03-10 14:59:52 +0100

    Step 06: use slider for Top rest

--- telamaque-movies/src/components/TopMovies/TopRest30.js
+++ telamaque-movies/src/components/TopMovies/TopRest30.js
@@ -1,12 +1,60 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { Slide } from 'react-slideshow-image';
+import Config from '../../Config/Config';
+
+const useStyles = makeStyles(theme => ({
+    slideContainer: {
+        width: '70%',
+        margin: 'auto'
+    },
+    slideElement: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundSize: 'cover',
+        height: 300
+    },
+    slideTitle: {
+        padding: 20,
+        fontSize: 20,
+        backgroundColor: '#efefef',
+        textAlign: 'center'
+    }
+  }));
+
+const properties = {
+    duration: 5000,
+    transitionDuration: 500,
+    infinite: true,
+    indicators: true,
+    arrows: true,
+    onChange: (oldIndex, newIndex) => {
+      console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+    }
+  }
+
 
 const TopRest30 = ({data}) => {
 
+    const classes = useStyles();
+
+
     return <div>
         <h1>Top rest 30 movies</h1>
-        <ul>
-            {data.map(movie => <li key={movie.id}>{movie.id}: {movie.title}</li>)}
-        </ul>
+        <div className={classes.slideContainer}>
+                <Slide {...properties}>
+                    {data.map(movie => 
+                        <div>
+                            <div className={classes.slideElement} style={{'backgroundImage': `url(${Config.ApiImages}/${movie.poster_path})`}}>
+                            <span className={classes.slideTitle}>{movie.title}</span>
+                        </div>
+                        </div>
+                    )}
+                </Slide>
+        </div>
+
     </div>;
 
 };
